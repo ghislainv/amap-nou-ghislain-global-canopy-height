a3e7c48f11d8b0f6a9183f131d5f9f70307d5c2d
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2025-06-15 08:29:19 +0200

    Get chm from meta at 1m resolution

--- code/meta_with_python_grass.py
+++ code/meta_with_python_grass.py
@@ -25,6 +25,15 @@ opd = os.path.dirname
 # GDAL exceptions
 gdal.UseExceptions()
 
+# Verbose
+verbose = True
+
+# Working directory
+wd = "/home/ghislain/Code/amap-nou-ghislain-global-canopy-height/"
+
+# Output directory
+os.makedirs(opj(wd, "outputs", "chm-meta"))
+
 # Check where GRASS python packages are and add them to PATH
 cmd = ["grass", "--config", "python_path"]
 python_path = subprocess.check_output(
@@ -40,7 +49,6 @@ grass_path = subprocess.check_output(
 import grass.script as gs  # noqa: E402
 
 # Create a project in the temporary directory
-wd = "/home/ghislain/Code/amap-nou-ghislain-global-canopy-height/"
 if not os.path.isdir(opj(wd, "grassdata", "meta_ch")):
     gs.create_project(path=opj(wd, "grassdata"),
                       name="meta_ch",
@@ -53,6 +61,16 @@ gs.setup.init(
     location="meta_ch", mapset="PERMANENT")
 gs.run_command(
     "g.region", flags="p")
+
+# Some info on grass environment
+gs.message("Current GRASS environment:")
+print(gs.gisenv())
+gs.message("Available raster maps:")
+for raster in gs.list_strings(type="raster"):
+    print(raster)
+gs.message("Available vector maps:")
+for vector in gs.list_strings(type="vector"):
+    print(vector)
 
 # Get a list of the files and directories available on AWS
 cmd = ["aws", "s3", "ls",
@@ -74,7 +92,7 @@ gs.run_command(
     overwrite=True)
 
 # Download geojson from aws
-ofile = opj(wd, "gisdata", "tiles.geojson")
+ofile = opj(wd, "output", "chm-meta", "tiles.geojson")
 cmd = ["aws", "s3", "cp",
        "--no-sign-request",
        ("s3://dataforgood-fb-data/forests/v1/"
@@ -107,9 +125,9 @@ qk = [i for i in qk if i]
 # Download files
 base_url = ("s3://dataforgood-fb-data/forests/"
             "v1/alsgedi_global_v6_float/chm")
-for i, quad in enumerate(qk):
+for _, quad in enumerate(qk):
     layername = f"tile_{quad}.tif"
-    ofile = opj(wd, "gisdata", layername)
+    ofile = opj(wd, "output", "chm-meta", layername)
     if not os.path.isfile(ofile):
         ifile = opj(base_url, f"{quad}.tif")
         cmd = ["aws", "s3", "cp", "--no-sign-request",
@@ -117,12 +135,11 @@ for i, quad in enumerate(qk):
         subprocess.run(cmd)
 
 # Create a virtual raster file
-tile_files = glob(opj(wd, "gisdata", "tile_*.tif"))
+tile_files = glob(opj(wd, "output", "chm-meta", "tile_*.tif"))
 
 # Callback
-verbose = True
 cback = gdal.TermProgress_nocb if verbose else 0
-vrt_file = opj(wd, "gisdata", "chm.vrt")
+vrt_file = opj(wd, "output", "chm-meta", "chm.vrt")
 chm_vrt = gdal.BuildVRT(
     vrt_file,
     tile_files,
@@ -131,36 +148,11 @@ chm_vrt = gdal.BuildVRT(
 chm_vrt.FlushCache()
 chm_vrt = None
 
-# # VRT to GeoTIFF
-# copts = ["COMPRESS=DEFLATE", "PREDICTOR=2",
-#          "NUM_THREADS=ALL_CPUS",
-#          "BIGTIFF=YES"]
-# wopts = ["CUTLINE_ALL_TOUCHED=TRUE", "NUM_THREADS=ALL_CPUS"]
-# ofile = opj(wd, "gisdata", "chm_meta.tif")
-# gdal.SetConfigOption("GDAL_NUM_THREADS", "ALL_CPUS")
-# gdal.SetConfigOption("GDAL_CACHEMAX, "4096")
-# gdal.Warp(ofile, vrt_file,
-#           cropToCutline=True,
-#           cutlineDSName=borders_file,
-#           outputType=gdal.GDT_Byte,
-#           dstSRS="EPSG:32758",
-#           dstNodata=255,
-#           resampleAlg="bilinear",
-#           xRes=1.2, yRes=1.2,
-#           targetAlignedPixels=True,
-#           warpOptions=wopts,
-#           multithread=True,
-#           creationOptions=copts,
-#           callback=cback)
-
-# Import files into grass
-# It takes some time without the -r flag.
+# Import files into grass with r.external
 gs.run_command(
     "r.external",
     input=vrt_file,
     output="chm_meta",
-    # overwrite=True,
-    # flags="r",
 )
 
 # Set color
@@ -182,11 +174,6 @@ with open(rules_file, "w") as f:
         f.write(f"{value} {color}\n")
 gs.run_command("r.colors", map="chm_meta", rules=rules_file)
 
-# # Install the r.clip addon
-# # Package grass-dev needs to be installed on the OS.
-# gs.run_command(
-#     "g.extension", extension="r.clip")
-
 # Set the region to match the extent of the area of interest
 gs.run_command(
     "g.region",
@@ -195,42 +182,176 @@ gs.run_command(
     flags="p")
 
 # Set a mask to mask out all areas outside the areas of interest
-# gs.run_command("g.list", type="raster")
-# gs.run_command("r.info", map="chm_meta")
-# gs.run_command("g.list", type="vect")
-# gs.run_command("r.mask", flags="r")
 gs.run_command(
     "r.mask",
     vector="borders_newcal",
     quiet=True,
     overwrite=True)
 
-# Clip the map
-# gs.run_command(
-#     "r.external.out",
-#     directory=opj(wd, "gisdata"),
-#     format="GTiff",
-#     options="BIGTIFF=YES,COMPRESS=DEFLATE",
-#     flags="p")
-# gs.run_command(
-#     "r.clip",
-#     input="chm_meta",
-#     output="chm_meta_clip",
-#     verbose=True)
-
-# # Cease GDAL output connection
-# gs.run_command(
-#     "r.external.out",
-#     flags="r")
-
-# Export as GeoTiff file
+# Export (takes about 1h30)
+ofile = opj(wd, "output", "chm-meta", "chm-meta.tif")
 gs.run_command(
     "r.out.gdal",
     input="chm_meta",
-    output=opj(wd, "gisdata", "chm_meta.tif"),
+    output=ofile,
     format="GTiff",
     type="Byte",
-    createopt=["BIGTIFF=YES,COMPRESS=DEFLATE"],
-    verbose=True)
+    createopt="COMPRESS=DEFLATE,PREDICTOR=2",
+    nodata=255,
+    verbose=True,
+    overwrite=True)
+
+# Reprojecting with GDAL (takes about 1h30).
+cback = gdal.TermProgress_nocb if verbose else 0
+input_file = opj(wd, "output", "chm-meta", "chm-meta.tif")
+output_file = opj(wd, "output", "chm-meta", "chm-meta-utm58s.tif")
+if (os.path.isfile(output_file)):
+    os.remove(output_file)
+copts = ["COMPRESS=DEFLATE", "PREDICTOR=2", "BIGTIFF=IF_NEEDED"]
+wopts = ["NUM_THREADS=ALL_CPUS"]
+with gdal.config_options(
+    {"GDAL_NUM_THREADS": "ALL_CPUS",
+     "GDAL_CACHEMAX": "1024"}):
+    gdal.Warp(
+        output_file, input_file,
+        xRes=1.2,
+        yRes=1.2,
+        targetAlignedPixels=True,
+        srcNodata=255,
+        dstNodata=255,
+        dstSRS="EPSG:32758",
+        resampleAlg=gdal.GRA_Bilinear,
+        outputType=gdal.GDT_Byte,
+        multithread=True,
+        creationOptions=copts,
+        warpMemoryLimit=500,
+        warpOptions=wopts,
+        callback=cback)
+
+# Convert to COG (relatively fast, 20min)
+cback = gdal.TermProgress_nocb if verbose else 0
+input_file = opj(wd, "output", "chm-meta", "chm-meta-utm58s.tif")
+output_file = opj(wd, "output", "chm-meta", "chm-meta-utm58s-cog.tif")
+copts = ["COMPRESS=DEFLATE", "PREDICTOR=2", "BIGTIFF=IF_NEEDED"]
+with gdal.config_options(
+    {"GDAL_NUM_THREADS": "ALL_CPUS",
+     "GDAL_CACHEMAX": "1024"}):
+    gdal.Translate(
+        output_file, input_file,
+        format="COG",
+        creationOptions=copts,
+        callback=cback)
+
+# ======================================
+# Acquisition dates
+# ======================================
+
+# Quadkeys
+qk = gs.read_command(
+    "v.db.select",
+    flags="c",
+    map="tiles_newcal",
+    columns="tile",
+    format="plain",
+    separator="comma",
+).split("\n")
+
+# Remove the empty strings
+qk = [i for i in qk if i]
+
+# Download
+baseurl = ("s3://dataforgood-fb-data/forests/v1/"
+           "alsgedi_global_v6_float/metadata")
+for _, quad in enumerate(qk):
+    layername = f"tile_{quad}"
+    ofile = opj(wd, "output", "chm-meta", f"{layername}.geojson")
+    if not os.path.isfile(ofile):
+        ifile = opj(baseurl, f"{quad}.geojson")
+        cmd = ["aws", "s3", "cp", "--no-sign-request",
+               ifile, ofile]
+        subprocess.run(cmd)
+    gs.run_command(
+        "v.import",
+        input=ofile,
+        output=layername)
+    os.remove(ofile)
+
+# Patch the files
+qkl = [f"tile_{x}" for x in qk]
+gs.run_command(
+    "v.patch",
+    flags="e",
+    input=qkl,
+    output="CHM_tmp1")
+gs.run_command(
+    "g.remove",
+    type="vector",
+    pattern="tile_*",
+    flags="f")
+
+# Convert column with dates to string
+# Required for the r.dissolve function
+gs.run_command(
+    "v.db.addcolumn",
+    map="CHM_tmp1",
+    columns="acq_strdate varchar(24)")
+gs.run_command(
+    "db.execute",
+    sql=("UPDATE CHM_tmp1 SET "
+         "acq_strdate = CAST(acq_date "
+         "AS VARCHAR(10))"))
+gs.run_command(
+    "v.db.dropcolumn",
+    map="CHM_tmp1",
+    columns="acq_date")
+
+# Dissolve areas with same acquire date
+gs.run_command(
+    "v.dissolve",
+    input="CHM_tmp1",
+    column="acq_strdate",
+    output="CHM_tmp2")
+
+# Create a new column holding the dates as date
+gs.run_command(
+    "v.db.addcolumn",
+    map="CHM_tmp2",
+    columns="acq_date date")
+gs.run_command(
+    "db.execute",
+    sql="UPDATE CHM_tmp2 SET acq_date = acq_strdate")
+
+# Clip to AOI
+gs.run_command(
+    "v.clip",
+    input="CHM_tmp2",
+    clip="borders_newcal",
+    output="CHM_acq_dates")
+ofile = opj(wd, "output", "chm-meta", "chm-acq-dates.gpkg")
+gs.run_command(
+    "v.out.ogr",
+    format="GPKG",
+    input="CHM_acq_dates",
+    output=ofile)
+gs.run_command(
+    "g.remove",
+    type="vector",
+    pattern="CHM_tmp*",
+    flags="f")
+
+# ====================================
+# Cleaning
+# ====================================
+
+# Cleaning
+os.remove(opj(wd, "output", "chm-meta", "chm.vrt"))
+os.remove(opj(wd, "output", "chm-meta", "chm_meta.tif"))
+os.remove(opj(wd, "output", "chm-meta", "chm_meta_utm58s.tif"))
+os.remove(opj(wd, "output", "chm-meta", "tiles.geojson"))
+for f in glob(opj(wd, "output", "chm-meta", "tile_*.tif")):
+    os.remove(f)
+
+# Removing grass project
+
 
 # End
